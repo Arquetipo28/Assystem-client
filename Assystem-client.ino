615a8f869e496bc80cf1a5bf8b0539b6008fc9ce
German Reynaga Araiza <german.reynagaa@gmail.com>
2019-05-26 00:28:24 -0500

    Including necessary libraries

---
+++ Assystem-client.ino
@@ -0,0 +1,14 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h> 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
