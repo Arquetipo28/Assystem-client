9c911738ecad0e43e96d97fc570c5480245c2755
German Reynaga Araiza <german.reynagaa@gmail.com>
2019-05-26 01:05:22 -0500

    Wifi send request to server

--- Assystem-client.ino
+++ Assystem-client.ino
@@ -14,6 +14,7 @@
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
+const char* host = "https://YOUR.WEB.SERVER/YOUR-ROUTE";
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
 void setup() {
   Serial.begin(9600);
@@ -47,7 +48,26 @@ void loop() {
       hex_num += mfrc522.uid.uidByte[3];
       Serial.println(hex_num);
       Serial.println();
+      String a = postValues(hex_num);
+      Serial.println(a);
+      Serial.println();
       mfrc522.PICC_HaltA();
     } 
   }
 }
+
+ String postValues(unsigned long card) {  
+    
+  HTTPClient http;   //Declare object of class HTTPClient    
+  http.begin(host); //Specify request destination
+  http.addHeader("Content-Type", "application/json"); //Specify content-type header  
+  http.addHeader("Accept", "application/json");
+  Serial.println(card);
+  String json = "{\"uid\":\"" + String(card) + "\", \"token\":\"OP-ASSYSTEM-AP2\"}";
+  int httpCode = http.POST(json);  //Send the request
+  String payload = http.getString(); //Get the response payload 
+  Serial.print("Response: ");
+  Serial.print(httpCode);
+  Serial.println();
+  return payload; 
+ }
