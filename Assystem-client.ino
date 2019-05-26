6b1c1bbd8001949b6e1cd1eb4e5cf0f046585dd3
German Reynaga Araiza <german.reynagaa@gmail.com>
2019-05-26 00:36:01 -0500

    Verifying tag read and printing UID

--- Assystem-client.ino
+++ Assystem-client.ino
@@ -3,12 +3,32 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h> 
 
+#define SS_PIN D4
+#define RST_PIN D2
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Reading RFID");
+  Serial.println();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (mfrc522.PICC_IsNewCardPresent()) {
+    if (mfrc522.PICC_ReadCardSerial()) {
+      Serial.print("UID: ");
+      // Answer from Hans Kilian in https://stackoverflow.com/questions/32839396/how-to-get-the-uid-of-rfid-in-arduino
+      unsigned long hex_num;
+      hex_num =  mfrc522.uid.uidByte[0] << 24;
+      hex_num += mfrc522.uid.uidByte[1] << 16;
+      hex_num += mfrc522.uid.uidByte[2] <<  8;
+      hex_num += mfrc522.uid.uidByte[3];
+      Serial.println(hex_num);
+      Serial.println();
+      mfrc522.PICC_HaltA();
+    } 
+  }
 }
